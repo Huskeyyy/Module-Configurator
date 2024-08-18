3d379416749a41d72204a4f6ed14a3eecfe7be8b
Tom <Tom@LAPTOP-VVQFI8G>
2024-08-18 19:22:50 +0100

    Add project files.

---
+++ Module-Configurator/MainForm.Designer.cs
@@ -0,0 +1,228 @@
+﻿namespace Module_Configurator
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            moduleDataGrid = new DataGridView();
+            moduleName = new DataGridViewTextBoxColumn();
+            baseAddress = new DataGridViewTextBoxColumn();
+            moduleSize = new DataGridViewTextBoxColumn();
+            menuStrip1 = new MenuStrip();
+            btnConnect = new ToolStripMenuItem();
+            btnRefreshModules = new Button();
+            statusStrip1 = new StatusStrip();
+            toolStripStatusLabel1 = new ToolStripStatusLabel();
+            lblNoOfModules = new ToolStripStatusLabel();
+            lblDivider1 = new ToolStripStatusLabel();
+            toolStripStatusLabel3 = new ToolStripStatusLabel();
+            lblPreviousModule = new ToolStripStatusLabel();
+            btnClear = new Button();
+            btnInjectConsole = new Button();
+            btnInjectPC = new Button();
+            ((System.ComponentModel.ISupportInitialize)moduleDataGrid).BeginInit();
+            menuStrip1.SuspendLayout();
+            statusStrip1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // moduleDataGrid
+            // 
+            moduleDataGrid.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            moduleDataGrid.Columns.AddRange(new DataGridViewColumn[] { moduleName, baseAddress, moduleSize });
+            moduleDataGrid.Location = new Point(12, 31);
+            moduleDataGrid.Name = "moduleDataGrid";
+            moduleDataGrid.RowHeadersVisible = false;
+            moduleDataGrid.RowHeadersWidth = 51;
+            moduleDataGrid.Size = new Size(789, 285);
+            moduleDataGrid.TabIndex = 0;
+            // 
+            // moduleName
+            // 
+            moduleName.HeaderText = "Name of Module";
+            moduleName.MinimumWidth = 6;
+            moduleName.Name = "moduleName";
+            moduleName.Width = 250;
+            // 
+            // baseAddress
+            // 
+            baseAddress.HeaderText = "Base Address";
+            baseAddress.MinimumWidth = 6;
+            baseAddress.Name = "baseAddress";
+            baseAddress.Width = 250;
+            // 
+            // moduleSize
+            // 
+            moduleSize.HeaderText = "Module Size";
+            moduleSize.MinimumWidth = 6;
+            moduleSize.Name = "moduleSize";
+            moduleSize.Width = 250;
+            // 
+            // menuStrip1
+            // 
+            menuStrip1.ImageScalingSize = new Size(20, 20);
+            menuStrip1.Items.AddRange(new ToolStripItem[] { btnConnect });
+            menuStrip1.Location = new Point(0, 0);
+            menuStrip1.Name = "menuStrip1";
+            menuStrip1.Size = new Size(813, 28);
+            menuStrip1.TabIndex = 1;
+            menuStrip1.Text = "menuStrip1";
+            // 
+            // btnConnect
+            // 
+            btnConnect.Name = "btnConnect";
+            btnConnect.Size = new Size(77, 24);
+            btnConnect.Text = "Connect";
+            btnConnect.Click += btnConnect_Click;
+            // 
+            // btnRefreshModules
+            // 
+            btnRefreshModules.Location = new Point(47, 322);
+            btnRefreshModules.Name = "btnRefreshModules";
+            btnRefreshModules.Size = new Size(175, 49);
+            btnRefreshModules.TabIndex = 2;
+            btnRefreshModules.Text = "Refresh Module List";
+            btnRefreshModules.UseVisualStyleBackColor = true;
+            btnRefreshModules.Click += btnRefreshModules_Click;
+            // 
+            // statusStrip1
+            // 
+            statusStrip1.ImageScalingSize = new Size(20, 20);
+            statusStrip1.Items.AddRange(new ToolStripItem[] { toolStripStatusLabel1, lblNoOfModules, lblDivider1, toolStripStatusLabel3, lblPreviousModule });
+            statusStrip1.Location = new Point(0, 382);
+            statusStrip1.Name = "statusStrip1";
+            statusStrip1.Size = new Size(813, 26);
+            statusStrip1.TabIndex = 3;
+            statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            toolStripStatusLabel1.Size = new Size(114, 20);
+            toolStripStatusLabel1.Text = "No. of Modules:";
+            // 
+            // lblNoOfModules
+            // 
+            lblNoOfModules.Name = "lblNoOfModules";
+            lblNoOfModules.Size = new Size(134, 20);
+            lblNoOfModules.Text = "<ModuleNumber>";
+            // 
+            // lblDivider1
+            // 
+            lblDivider1.Name = "lblDivider1";
+            lblDivider1.Size = new Size(13, 20);
+            lblDivider1.Text = "|";
+            // 
+            // toolStripStatusLabel3
+            // 
+            toolStripStatusLabel3.Name = "toolStripStatusLabel3";
+            toolStripStatusLabel3.Size = new Size(187, 20);
+            toolStripStatusLabel3.Text = "Previously Loaded Module:";
+            // 
+            // lblPreviousModule
+            // 
+            lblPreviousModule.Name = "lblPreviousModule";
+            lblPreviousModule.Size = new Size(135, 20);
+            lblPreviousModule.Text = "<PreviousModule>";
+            // 
+            // btnClear
+            // 
+            btnClear.Location = new Point(228, 322);
+            btnClear.Name = "btnClear";
+            btnClear.Size = new Size(175, 49);
+            btnClear.TabIndex = 4;
+            btnClear.Text = "Clear Module List";
+            btnClear.UseVisualStyleBackColor = true;
+            btnClear.Click += btnClear_Click;
+            // 
+            // btnInjectConsole
+            // 
+            btnInjectConsole.Location = new Point(409, 322);
+            btnInjectConsole.Name = "btnInjectConsole";
+            btnInjectConsole.Size = new Size(175, 49);
+            btnInjectConsole.TabIndex = 6;
+            btnInjectConsole.Text = "Inject Module from Console";
+            btnInjectConsole.UseVisualStyleBackColor = true;
+            btnInjectConsole.Click += btnInjectConsole_Click;
+            // 
+            // btnInjectPC
+            // 
+            btnInjectPC.Location = new Point(590, 322);
+            btnInjectPC.Name = "btnInjectPC";
+            btnInjectPC.Size = new Size(175, 49);
+            btnInjectPC.TabIndex = 5;
+            btnInjectPC.Text = "Inject Module from PC";
+            btnInjectPC.UseVisualStyleBackColor = true;
+            btnInjectPC.Click += btnInjectPC_Click;
+            // 
+            // MainForm
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(813, 408);
+            Controls.Add(btnInjectConsole);
+            Controls.Add(btnInjectPC);
+            Controls.Add(btnClear);
+            Controls.Add(statusStrip1);
+            Controls.Add(btnRefreshModules);
+            Controls.Add(moduleDataGrid);
+            Controls.Add(menuStrip1);
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            MainMenuStrip = menuStrip1;
+            MaximizeBox = false;
+            MinimizeBox = false;
+            Name = "MainForm";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Module Configurator";
+            ((System.ComponentModel.ISupportInitialize)moduleDataGrid).EndInit();
+            menuStrip1.ResumeLayout(false);
+            menuStrip1.PerformLayout();
+            statusStrip1.ResumeLayout(false);
+            statusStrip1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private DataGridView moduleDataGrid;
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem btnConnect;
+        private Button btnRefreshModules;
+        private StatusStrip statusStrip1;
+        private ToolStripStatusLabel toolStripStatusLabel1;
+        private ToolStripStatusLabel lblNoOfModules;
+        private ToolStripStatusLabel lblDivider1;
+        private ToolStripStatusLabel toolStripStatusLabel3;
+        private ToolStripStatusLabel lblPreviousModule;
+        private Button btnClear;
+        private DataGridViewTextBoxColumn moduleName;
+        private DataGridViewTextBoxColumn baseAddress;
+        private DataGridViewTextBoxColumn moduleSize;
+        private Button btnInjectConsole;
+        private Button btnInjectPC;
+    }
+}
---
+++ Module-Configurator/MainForm.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.ComponentModel;
+using XDevkit;
+using JRPC_Client;
+
+namespace Module_Configurator
+{
+    public partial class MainForm : Form
+    {
+        private IXboxConsole console; // Represents the Xbox console instance
+
+        public MainForm()
+        {
+            InitializeComponent(); // Initializes the form components
+        }
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Attempt to connect to the console using default settings
+                if (console.Connect(out console, "default"))
+                {
+                    // Connect to the console's debugger
+                    console.DebugTarget.ConnectAsDebugger("jtag", XboxDebugConnectFlags.Force);
+                    // Refresh the list of modules
+                    btnRefreshModules_Click(sender, e);
+                }
+                else
+                {
+                    // Show error if connection fails
+                    MessageBox.Show("Failed to connect to the console.", "Connection Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            catch (Exception ex)
+            {
+                // Show error message if an exception occurs
+                MessageBox.Show($"An error occurred: {ex.Message}", "Module Configurator", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                Close(); // Close the application if there's an error
+            }
+        }
+
+        private void btnRefreshModules_Click(object sender, EventArgs e)
+        {
+            moduleDataGrid.Rows.Clear(); // Clear existing rows
+
+            // Loop through each module in the console's debugger target
+            foreach (IXboxModule xboxModule in console.DebugTarget.Modules)
+            {
+                string name = xboxModule.ModuleInfo.Name;
+                string baseAddressHex = $"0x{xboxModule.ModuleInfo.BaseAddress:X}";
+                string sizeHex = $"0x{xboxModule.ModuleInfo.Size:X}";
+
+                // Create a new row for the module and add it to the data grid
+                object[] row = { name, baseAddressHex, sizeHex };
+                moduleDataGrid.Rows.Add(row);
+            }
+
+            // Sort the data grid by base address in ascending order
+            moduleDataGrid.Sort(moduleDataGrid.Columns[1], ListSortDirection.Ascending);
+            lblNoOfModules.Text = moduleDataGrid.Rows.Count.ToString(); // Update the label with the count of modules
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            moduleDataGrid.Rows.Clear(); // Clear all rows from the data grid
+        }
+
+        private void btnInjectPC_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.Filter = "All Files (*.*)|*.*";
+                openFileDialog.Title = "Select a File";
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    string fileName = Path.GetFileName(openFileDialog.FileName);
+                    string filePath = Path.Combine("Hdd:\\", fileName); // Define the file path on the Xbox console
+
+                    try
+                    {
+                        // Send the selected file to the Xbox console
+                        console.SendFile(openFileDialog.FileName, filePath);
+                        // Call the Xbox kernel to inject the module
+                        console.Call<uint>("xboxkrnl.exe", 409, new object[]
+                        {
+                            filePath,
+                            8,
+                            0,
+                            0
+                        });
+
+                        // Refresh the list of modules and update UI labels
+                        btnRefreshModules_Click(sender, e);
+                        lblNoOfModules.Text = moduleDataGrid.Rows.Count.ToString();
+                        lblPreviousModule.Text = fileName; // Set the file name label
+                        MessageBox.Show("Successfully injected module!");
+                    }
+                    catch (Exception ex)
+                    {
+                        // Show error message if the injection fails
+                        MessageBox.Show($"Failed to inject module: {ex.Message}", "Injection Error", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+                    }
+                }
+            }
+        }
+
+        private void btnInjectConsole_Click(object sender, EventArgs e)
+        {
+            string modulePath = string.Empty; // Variable to hold the module path
+
+            // Show the InputBox to get the module path
+            if (InputBox.Show("Test Input Box",
+                "&Enter the path of the module you would like to load:", ref modulePath) == DialogResult.OK)
+            {
+                try
+                {
+                    // Calls the Xbox kernel to inject the module using the provided path
+                    console.Call<uint>("xboxkrnl.exe", 409, new object[]
+                    {
+                        modulePath,
+                        8,
+                        0,
+                        0
+                    });
+
+                    // Refresh modules and update labels
+                    btnRefreshModules_Click(sender, null);
+                    lblNoOfModules.Text = moduleDataGrid.Rows.Count.ToString();
+                    lblPreviousModule.Text = modulePath; // Update the previous module label
+                    console.XNotify("Successfully injected module!");
+                }
+                catch (Exception)
+                {
+                    // Show error message if the injection fails
+                    MessageBox.Show("Failed to inject module, maybe the module you want to inject does not support it.", "Injection Error", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+                }
+            }
+            else
+            {
+                // If user cancels or closes the InputBox, exit the method
+                return;
+            }
+        }
+    }
+}
